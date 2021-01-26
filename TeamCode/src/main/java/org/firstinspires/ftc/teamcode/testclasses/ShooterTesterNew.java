5bf993c8a09c6fa8f69ba7667cb5a92c0d565a35
Benjamin Colby <bcolby@gmail.com>
2021-01-25 20:04:12 -0800

    fixing bugs with shooter test class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testclasses/ShooterTesterNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testclasses/ShooterTesterNew.java
@@ -5,13 +5,21 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 @TeleOp(name = "shooterTester",group = "TeleOp")
 public class ShooterTesterNew extends LinearOpMode {
     public void runOpMode() {
+        double timeInitial = 0;
+        double deltaTime = 0;
+        double deltaPosition = 0;
+        double positionInitial = 0;
+        ElapsedTime time;
+        time = new ElapsedTime();
         DcMotorEx shooter1 = hardwareMap.get(DcMotorEx.class, "shooterMotor1");
-        DcMotor shooter2 = hardwareMap.get(DcMotor.class, "shooterMotor1");
+        DcMotor shooter2 = hardwareMap.get(DcMotor.class, "shooterMotor2");
+        shooter1.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         shooter1.setDirection(DcMotorSimple.Direction.FORWARD);
         shooter2.setDirection(DcMotorSimple.Direction.FORWARD);
         waitForStart();
@@ -20,8 +28,13 @@ public class ShooterTesterNew extends LinearOpMode {
             shooter1.setPower(shooterPower);
             shooter2.setPower(shooterPower);
             shooterPower = gamepad1.right_stick_y;
-            telemetry.addData("velocity: ", shooter1.getVelocity());
+            deltaTime = time.milliseconds() - timeInitial;
+            deltaPosition = shooter1.getCurrentPosition() - positionInitial;
+            telemetry.addData("Shooter Velo: ", deltaPosition/deltaTime);
             telemetry.update();
+            timeInitial = time.milliseconds();
+            positionInitial = shooter1.getCurrentPosition();
+            sleep(100);
         }
     }
 }
