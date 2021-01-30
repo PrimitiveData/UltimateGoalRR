e11d2cdd125f0560e057d71a84021ea1032fac0c
Benjamin Colby <bcolby@gmail.com>
2021-01-30 14:54:56 -0800

    intake and wobble arm tester classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testclasses/IntakeTester.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.testclasses;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.hardware.HardwareMecanum;
+
+@TeleOp(name = "IntakeTester",group = "TeleOp")
+public class IntakeTester extends LinearOpMode{
+        public void runOpMode(){
+            HardwareMecanum hardware = new HardwareMecanum(hardwareMap, telemetry);
+            waitForStart();
+            while(!isStopRequested()){
+                hardware.intake.turnIntake(gamepad1.right_stick_y);
+            }
+        }
+    }
