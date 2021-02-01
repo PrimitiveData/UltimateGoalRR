cb775fff36d4f09b7b380c0c5d1bc4c5424f21a6
Benjamin Colby <bcolby@gmail.com>
2021-01-31 16:12:31 -0800

    turret tester class with tracking + rotation control in mag states

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareComponents/Mag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareComponents/Mag.java
@@ -1,14 +1,17 @@
 package org.firstinspires.ftc.teamcode.hardware.HardwareComponents;
 
+import org.firstinspires.ftc.teamcode.hardware.HardwareMecanum;
 import org.firstinspires.ftc.teamcode.hardware.RegServo;
 
 public class Mag {
     public RegServo magServo;
     RegServo ringPusher;
+    HardwareMecanum hardware;
     double ringPusherResting=0.38589; // must be tuned
     double ringPusherPushedIn=0.1425627; // must be tuned
     double magCollectRingPosition=0.045;
     double magDropRingPosition=0.39217;
+    double magRotationCollectPosition = 0.48;
     public State currentState;
     public Mag(RegServo magServo, RegServo ringPusher){
         this.magServo = magServo;
@@ -16,10 +19,13 @@ public class Mag {
         currentState = State.COLLECT;
     }
     public void collectRings(){
+        hardware.turret.magShootingState = false;
+        hardware.turret.setMagAngle(magCollectRingPosition);
         currentState = State.COLLECT;
         magServo.setPosition(magCollectRingPosition);
     }
     public void dropRings(){
+        hardware.turret.magShootingState = true;
         currentState = State.DROP;
         magServo.setPosition(magDropRingPosition);
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testclasses/TurretTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testclasses/TurretTester.java
@@ -11,7 +11,58 @@ public class TurretTester extends LinearOpMode {
         HardwareMecanum hardware = new HardwareMecanum(hardwareMap, telemetry);
         waitForStart();
         while(!isStopRequested()){
-            hardware.turret.turretMotor.setPower(gamepad1.right_stick_y);
+            boolean turretTracking = false;
+            boolean magTracking = false;
+            double globalTurretAngle = 0;
+            boolean aToggle = false;
+            boolean bToggle = false;
+
+            if(gamepad1.dpad_left)
+                globalTurretAngle += 20;
+            if(gamepad1.dpad_right)
+                globalTurretAngle -= 20;
+
+            if(gamepad1.a) {
+                if(!aToggle)
+                    turretTracking = !turretTracking;
+                aToggle = true;
+            }
+            else{
+                if(aToggle)
+                    aToggle = false;
+            }
+
+            if(gamepad1.b) {
+                if(!bToggle)
+                    magTracking = !magTracking;
+                bToggle = true;
+            }
+            else{
+                if(bToggle)
+                    bToggle = false;
+            }
+
+            if(turretTracking){
+                hardware.turret.updatePID = true;
+                hardware.turret.setTurretAngle(globalTurretAngle);
+            }
+            else {
+                hardware.turret.updatePID = false;
+                hardware.turret.setTurretMotorPower(0);
+            }
+            if(magTracking) {
+                hardware.mag.dropRings();
+                hardware.turret.setTurretAngle(globalTurretAngle);
+            }
+            else
+                hardware.mag.collectRings();
+
+            telemetry.addData("Global Turret Angle: ", globalTurretAngle);
+            telemetry.addData("Turret Current Local Position: ", hardware.turret.localTurretAngleRadians());
+            telemetry.addData("Robot Heading: ", hardware.getAngle());
+            telemetry.addData("Turret Tracking: ", turretTracking);
+            telemetry.addData("Mag Tracking: ", magTracking);
+            telemetry.update();
             hardware.loop();
         }
     }
