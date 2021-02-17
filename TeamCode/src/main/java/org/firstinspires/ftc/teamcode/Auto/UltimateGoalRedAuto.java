33511c42d6e1c86ec7132cc55d6dba877762650d
thomas <thomas11950@gmail.com>
2021-02-17 13:48:58 -0800

    -changed 180 to -179

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/UltimateGoalRedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/UltimateGoalRedAuto.java
@@ -103,7 +103,7 @@ public class UltimateGoalRedAuto extends AutoMethods {
 
         }else{
             collect2ndWobbler = hardware.drive.trajectoryBuilder(dropWobbler1.end())
-                    .lineToLinearHeading(new Pose2d(-10,36,Math.toRadians(180)))
+                    .lineToLinearHeading(new Pose2d(-10,36,Math.toRadians(-179)))
                     .build();
         }
 
